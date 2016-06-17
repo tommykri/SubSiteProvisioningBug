e787db025b0e102a1052b3da34b7bfecfbebe5f7
Tommy Kristoffersen <tommy.kristoffersen@bouvet.no>
2016-06-17 09:39:31 +0200

    Running against SharePoint Online

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint.Client;
 using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Utilities;
 
 namespace ApplyTemplateToSubSiteBug
 {
@@ -14,10 +16,14 @@ namespace ApplyTemplateToSubSiteBug
         {
             try
             {
-                string subSiteUrl = "http://yoursharepointsitecollection/subsite";
+                string subSiteUrl = "https://yoursharepointonlineurlwithsubsite";
+                var username = "username";
+                var password = "password";
 
                 using (var context = new ClientContext(subSiteUrl))
-                {  
+                {
+                    context.Credentials = new SharePointOnlineCredentials(username, EncryptionUtility.ToSecureString(password));
+
                     // Applying a empty template to a subsite gives exception: startIndex cannot be larger than length of string.\r\nParameter name: startIndex  
                     var template = new ProvisioningTemplate();
                     context.Web.ApplyProvisioningTemplate(template);
